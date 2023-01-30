28b463ed1945281f47630d451b7665084e45847c
Adam Tovatt <sporedenstore@hotmail.com>
2023-01-30 22:43:47 +0100

    will work on same network as server

--- src/Api.js
+++ src/Api.js
@@ -25,8 +25,26 @@ export async function GetChatResponse(message) {
   );
 }
 
+const serverIp = "92.34.13.93";
+let currentIp = null;
+
+export async function GetIsServer() {
+  if (!currentIp) {
+    let result = await (
+      await fetch("https://api.ipify.org?format=json", {
+        method: "GET",
+      })
+    ).json();
+    currentIp = result.ip;
+  }
+
+  if (serverIp === currentIp) return true;
+  return false;
+}
+
 export function GetBasePath() {
   let requestPath = "https://ledigasalar.online/betapet-bot-api";
+  if (GetIsServer()) requestPath = "https://192.168.1.89/auto-builder";
   if (!process.env.NODE_ENV || process.env.NODE_ENV === "development") {
     //use local address if development
     //requestPath = "https://localhost:5001";
